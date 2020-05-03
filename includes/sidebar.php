9959cf1542a1fc8635229e76b34eb49608a25e0b
Patrick Rizzardi <patrick.d.rizzardi@gmail.com>
2020-05-03 06:42:31 -0400

    fixed format issue after login removed

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -19,41 +19,39 @@ if (ifItIsMethod('post')) {
 }
 
 ?>
+<div class="col-md-4">
+    <!-- Blog Categories Well -->
+    <div class="well">
 
-<!-- Blog Categories Well -->
-<div class="well">
+        <?php
+        $query = "SELECT * FROM categories";
+        $select_categories_sidebar = mysqli_query($connection, $query);
+        ?>
+        <h4>Blog Categories</h4>
+        <div class="row">
+            <div class="col-lg-12">
+                <ul class="list-unstyled">
 
+                    <?php
 
+                    while ($row = mysqli_fetch_assoc($select_categories_sidebar)) {
+                        $cat_title = $row['cat_title'];
+                        $cat_id = $row['cat_id'];
 
-    <?php
-    $query = "SELECT * FROM categories";
-    $select_categories_sidebar = mysqli_query($connection, $query);
-    ?>
-    <h4>Blog Categories</h4>
-    <div class="row">
-        <div class="col-lg-12">
-            <ul class="list-unstyled">
+                        echo "<li><a href='category.php?category=$cat_id'>{$cat_title}</a></li>";
+                    }
 
-                <?php
+                    ?>
 
-                while ($row = mysqli_fetch_assoc($select_categories_sidebar)) {
-                    $cat_title = $row['cat_title'];
-                    $cat_id = $row['cat_id'];
+                </ul>
+            </div>
 
-                    echo "<li><a href='category.php?category=$cat_id'>{$cat_title}</a></li>";
-                }
-
-                ?>
-
-            </ul>
         </div>
-
+        <!-- /.row -->
     </div>
-    <!-- /.row -->
-</div>
 
-<!-- Side Widget Well -->
-<?php //include "widget.php"; 
-?>
+    <!-- Side Widget Well -->
+    <?php //include "widget.php"; 
+    ?>
 
 </div>
