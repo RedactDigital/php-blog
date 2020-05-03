d6fd5d099d815429b4adf5749bd819d5576d636d
Patrick Rizzardi <patrick.d.rizzardi@gmail.com>
2020-05-03 06:35:53 -0400

    navbar update

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,3 +1,31 @@
+<?php
+
+
+if(ifItIsMethod('post')){
+
+
+        if(isset($_POST['login'])){
+
+
+            if(isset($_POST['username']) && isset($_POST['password'])){
+
+                login_user($_POST['username'], $_POST['password']);
+
+
+            }else {
+
+
+                redirect('/');
+            }
+
+
+        }
+
+}
+
+?>
+   
+   
    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
        <div class="container">
 
