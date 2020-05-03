bea2328b81be19fe59ec4c8760bb3c924f24c3c5
Patrick Rizzardi <patrick.d.rizzardi@gmail.com>
2020-05-03 06:49:05 -0400

    css

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,24 +1,3 @@
-<?php
-
-
-if (ifItIsMethod('post')) {
-
-
-    if (isset($_POST['login'])) {
-
-
-        if (isset($_POST['username']) && isset($_POST['password'])) {
-
-            login_user($_POST['username'], $_POST['password']);
-        } else {
-
-
-            redirect('/');
-        }
-    }
-}
-
-?>
 <div class="col-md-4">
     <!-- Blog Categories Well -->
     <div class="well">
