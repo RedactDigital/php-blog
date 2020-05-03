5da378007d231f67ea3a273d23c36b022452d87c
patrick <patrick.d.rizzardi@gmail.com>
2020-05-03 10:16:53 +0000

    fix commit

--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -1,7 +1,37 @@
+<?php ob_start(); ?>
+<?php include "../includes/db.php"; ?>
+<?php include "functions.php"; ?>
+
+
+
 <?php session_start(); ?>
-<?php include "admin/functions.php"; ?>
+
+
+<?php
+
+
+
+
+if (isset($_SESSION['user_role'])) {
+} else {
+
+    header("location: /");
+}
+
+
+
+
+?>
+
+
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
+
 
 <head>
 
@@ -11,15 +41,16 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Blog | Created Digital | Physics | Technology | Lifestyle</title>
+    <title>Creative Digital | Admin Panel</title>
 
     <!-- Bootstrap Core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom CSS -->
-    <link href="css/blog-home.css" rel="stylesheet">
+    <link href="css/sb-admin.css" rel="stylesheet">
 
-    <link href="css/styles.css" rel="stylesheet">
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -27,6 +58,25 @@
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
+
+    <link href="css/styles.css" rel="stylesheet">
+
+
+    <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+
+
+
+    <!-- CKEditor 5 -->
+    <script src="../vendor/ckeditor/build/ckeditor.js"></script>
+
+    <!-- CKFinder -->
+    <script src="../vendor/ckfinder/ckfinder.js"></script>
+
+
+    <script src="js/jquery.js"></script>
+
+
+
 
 
 
