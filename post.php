7db2553228b45bc49c2b2433b4eb224c9015c0b1
Patrick Rizzardi <patrick.d.rizzardi@gmail.com>
2020-05-09 20:59:12 -0400

    fixing tags to say coming soon

--- post.php
+++ post.php
@@ -96,14 +96,14 @@ if (isset($_GET['p_id'])) {
                         <span class="s-content__tag-list">
                             <?php
 
-                            $query = "SELECT * FROM tags ";
+                            $query = "SELECT * FROM tags WHERE tag_id = '$the_post_id' ";
                             $select_all_tags_query = mysqli_query($connection, $query);
 
                             $row = mysqli_fetch_assoc($select_all_tags_query);
                             $post_tags = $row['tag_title'];
 
-                            echo "<a href='#0'> $post_tags</a>";
                             ?>
+                            <a href="#0"><?php echo $post_tags ?></a> 
                         </span>
                     </p> <!-- end s-content__tags -->
 
